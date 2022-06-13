6fe499b2d29a43c09922f0f83955af3fc0c7e5e8
andriusmw <andriusmw@yahoo.es>
2022-06-13 09:35:24 +0200

    ej1 solucion 3

--- dia1/main.js
+++ dia1/main.js
@@ -1,6 +1,18 @@
+
+/*
 const p = document.createElement("p");
 p.textContent = "hello world";
 p.className = "text";
 const body = document.querySelector("body")
 body.append(p);
 
+*/
+const root = document.querySelector("#root");
+const p = React.createElement("p",{
+className: "text",
+children: "Hello world2",
+});
+
+ReactDOM.render(p, root)
+//primer parámetro es lo que quieres pintar
+//segundo parámetro es donde lo quieres pintar
