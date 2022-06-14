be49467e64754b1bc78566afdeaa85e77d97322a
andriusmw <andriusmw@yahoo.es>
2022-06-14 10:19:24 +0200

    dia 2 ejercicio 1 tercera opcion optima

--- dia2/segunda/src/App.js
+++ dia2/segunda/src/App.js
@@ -1,32 +1,27 @@
+import "./App.css";
+import entries from "./entries.json";
 
-import './App.css';
-import entries from "./entries.json"
+/** Componene Entry que pinte los datos de una entrada sacada del JSON */
 
+const Entry = ({ data }) => {
+  const { id, title, description, userName, photo } = data;
 
-const Entry = ({ id, title, description, userName, photo }) => {
   return (
-   <article id={id} >
-     <h2> {title}</h2>
-     <img src={photo} alt={title} />
-     <p> {description}</p>
-     <p>Created by: {userName}</p>
-   </article>
+    <article id={id}>
+      <h2>{title}</h2>
+      <img src={photo} alt={title} />
+      <p>{description}</p>
+      <p>Created by: {userName}</p>
+    </article>
   );
 };
 
-
 function App() {
   return (
-   <div className="App">
-    <Entry 
-      id={entries[0].id}
-      title={entries[0].title}
-      description={entries[0].description}
-      photo={entries[0].photo}
-      userName={entries[0].userName}
-      ></Entry>
-     <Entry >{entries[1].title}</Entry>
-   </div>
+    <div className="App">
+      <Entry data={entries[0]} />
+      <Entry data={entries[1]} />
+    </div>
   );
 }
 
